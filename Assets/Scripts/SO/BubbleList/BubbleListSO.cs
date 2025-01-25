b62c678149d7ec391e943cebfdda3189bcf06867
Sherwin <87555304+owenlim225@users.noreply.github.com>
2025-01-25 16:46:05 +0800

    Revert "Tite example"

--- Assets/Scripts/SO/BubbleList/BubbleListSO.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BubbleListSO", menuName = "ScriptableObjects/BubbleListSO", order = 0)]
-public class BubbleListSO : ScriptableObject {
-    public List<BubbleSO> bubbleObject;
-}
--- Assets/Scripts/SO/BubbleList/BubbleSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "BubbleSO", menuName = "ScriptableObjects/BubbleSO", order = 2)]
-public class BubbleSO : ScriptableObject {
-    public GameObject bubblePrefab;
-    public int scoreCount;
-}
